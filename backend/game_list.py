fd5602d6e43a30e4f3835268c90450f99118d7de
Netanel Aker <netn10@gmail.com>
2023-11-20 22:26:11 +0200

    Update game_list.py

--- backend/game_list.py
+++ backend/game_list.py
@@ -1,5 +1,13 @@
 game_list = [
-    {"id": 1, "name": "Street Fighter", "playersInGame": 0, "playersInLobby": 0},
-    {"id": 2, "name": "Tekken", "playersInGame": 0, "playersInLobby": 0},
-    # Add more game data as needed
+    {"id": 1, "steam_id" : "1364780", "name": "Street Fighter 6"},
+    {"id": 2, "steam_id" : "1498570", "name": "THE KING OF FIGHTERS XV"},
+    {"id": 3, "steam_id" : "222440", "name": "THE KING OF FIGHTERS 2002 UNLIMITED MATCH"},
+    {"id": 4, "steam_id" : "222940", "name": "THE KING OF FIGHTERS XIII STEAM EDITION"},
+    {"id": 5, "steam_id" : "222420", "name": "THE KING OF FIGHTERS '98 ULTIMATE MATCH FINAL EDITION"},
+    {"id": 6, "steam_id" : "702120", "name": "THE KING OF FIGHTERS '97 GLOBAL MATCH"},
+    {"id": 7, "steam_id" : "1342260", "name": "SAMURAI SHODOWN"},
+    {"id": 8, "steam_id" : "999660", "name": "SAMURAI SHODOWN NEOGEO COLLECTION"},
+    {"id": 9, "steam_id" : "1076550", "name": "SAMURAI SHODOWN V SPECIAL / サムライスピリッツ零スペシャル"},
+    {"id": 10, "steam_id" : "356910", "name": "Battle Fantasia -Revised Edition-"},
+    {"id": 11, "steam_id" : "1076540", "name": "Checkmate Showdown"},
 ]
